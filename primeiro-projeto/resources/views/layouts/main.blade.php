ec373d6522081a08866596fe943c5ded2fedbe2a
vini-ayres <v.ayres@aluno.ifsp.edu.br>
2023-10-11 12:14:18 -0300

    commit

--- primeiro-projeto/resources/views/layouts/main.blade.php
+++ primeiro-projeto/resources/views/layouts/main.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
         
         <!-- CSS Bootstrap -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         
         <!-- CSS da aplicação -->
         <link rel="stylesheet" href="/css/styles.css">
@@ -20,26 +20,33 @@
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light"></nav>
-            <div class="collapse navbar-collapse" id="navbar">
-                <a href="/" class="navbar-brand">
-                    <img src="/img/icone.svg" alt="HDC Events">
-                </a>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="/" class="nav-link">Eventos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/events/create" class="nav-link">Criar Eventos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/" class="nav-link">Entrar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/" class="nav-link">Cadastrar</a>
-                    </li>
-                </ul>
-            </div>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container">
+                    <a class="navbar-brand" href="/">
+                        <img src="/img/conference.ico" alt="HDC Events" class="d-inline-block align-top">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbar">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/events/create">Criar Eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Entrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Cadastrar</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <img src="/img/banner1.jpg" alt="Banner 1" id="banner-1">
         </header>
         @yield('content')
         <footer>
